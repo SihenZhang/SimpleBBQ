38a5b827eb4e8a0b031fcfb468faaed5b941145b
SihenZhang <1334125013@qq.com>
2022-06-23 00:23:44 +0800

    Fix Bug of Skewering Table Model

--- src/main/java/com/sihenzhang/simplebbq/data/SimpleBBQBlockStateProvider.java
+++ src/main/java/com/sihenzhang/simplebbq/data/SimpleBBQBlockStateProvider.java
@@ -1,6 +1,7 @@
 package com.sihenzhang.simplebbq.data;
 
 import com.sihenzhang.simplebbq.SimpleBBQ;
+import com.sihenzhang.simplebbq.SimpleBBQRegistry;
 import com.sihenzhang.simplebbq.util.RLUtils;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
@@ -13,11 +14,16 @@ public class SimpleBBQBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        this.models().orientableWithBottom("skewering_table",
-                RLUtils.createRL("block/skewering_table_side"),
-                RLUtils.createRL("block/skewering_table_front"),
-                RLUtils.createRL("block/skewering_table_bottom"),
-                RLUtils.createVanillaRL("block/smooth_stone")
+        this.simpleBlock(SimpleBBQRegistry.GRILL_BLOCK.get(), this.models().getExistingFile(RLUtils.createRL("block/grill")));
+        this.horizontalBlock(SimpleBBQRegistry.SKEWERING_TABLE_BLOCK.get(),
+                this.models().cube("skewering_table",
+                        RLUtils.createRL("block/skewering_table_bottom"),
+                        RLUtils.createVanillaRL("block/smooth_stone"),
+                        RLUtils.createRL("block/skewering_table_front"),
+                        RLUtils.createRL("block/skewering_table_front"),
+                        RLUtils.createRL("block/skewering_table_side"),
+                        RLUtils.createRL("block/skewering_table_side")
+                ).texture("particle", RLUtils.createRL("block/skewering_table_front"))
         );
     }
 }
