9a21677f84ac23a9f03b8cb16d20f57df419dc8e
SihenZhang <1334125013@qq.com>
2022-07-01 00:49:07 +0800

    Fix bug in Village Structures Generation

--- src/main/java/com/sihenzhang/simplebbq/level/VillageStructures.java
+++ src/main/java/com/sihenzhang/simplebbq/level/VillageStructures.java
@@ -5,9 +5,11 @@ import com.sihenzhang.simplebbq.SimpleBBQ;
 import com.sihenzhang.simplebbq.util.RLUtils;
 import net.minecraft.Util;
 import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.structure.pools.SinglePoolElement;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorList;
 import net.minecraftforge.event.server.ServerAboutToStartEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,9 +18,13 @@ import java.util.ArrayList;
 
 @Mod.EventBusSubscriber(modid = SimpleBBQ.MOD_ID)
 public class VillageStructures {
+    private static final ResourceKey<StructureProcessorList> EMPTY_PROCESSOR_LIST_KEY = ResourceKey.create(Registry.PROCESSOR_LIST_REGISTRY, RLUtils.createVanillaRL("empty"));
+
     @SubscribeEvent
     public static void addNewVillageBuilding(final ServerAboutToStartEvent event) {
-        var templatePools = event.getServer().registryAccess().registry(Registry.TEMPLATE_POOL_REGISTRY).orElseThrow();
+        var registryHolder = event.getServer().registryAccess();
+        var templatePools = registryHolder.registryOrThrow(Registry.TEMPLATE_POOL_REGISTRY);
+        var processorLists = registryHolder.registryOrThrow(Registry.PROCESSOR_LIST_REGISTRY);
 
         var plainsPoolRL = RLUtils.createVanillaRL("village/plains/houses");
         var savannaPoolRL = RLUtils.createVanillaRL("village/savanna/houses");
@@ -26,21 +32,22 @@ public class VillageStructures {
         var taigaPoolRL = RLUtils.createVanillaRL("village/taiga/houses");
         var snowyPoolRL = RLUtils.createVanillaRL("village/snowy/houses");
 
-        addBuildingToPool(templatePools, plainsPoolRL, RLUtils.createRL("village/houses/plains_bbq_camp_1"), 1);
-        addBuildingToPool(templatePools, plainsPoolRL, RLUtils.createRL("village/houses/plains_bbq_camp_2"), 1);
-        addBuildingToPool(templatePools, savannaPoolRL, RLUtils.createRL("village/houses/savanna_bbq_camp_1"), 2);
-        addBuildingToPool(templatePools, desertPoolRL, RLUtils.createRL("village/houses/desert_bbq_camp_1"), 2);
-        addBuildingToPool(templatePools, taigaPoolRL, RLUtils.createRL("village/houses/taiga_bbq_camp_1"), 2);
-        addBuildingToPool(templatePools, snowyPoolRL, RLUtils.createRL("village/houses/snowy_bbq_camp_1"), 3);
-        addBuildingToPool(templatePools, snowyPoolRL, RLUtils.createRL("village/houses/snowy_bbq_camp_2"), 1);
+        addBuildingToPool(templatePools, processorLists, plainsPoolRL, RLUtils.createRL("village/houses/plains_bbq_camp_1"), 1);
+        addBuildingToPool(templatePools, processorLists, plainsPoolRL, RLUtils.createRL("village/houses/plains_bbq_camp_2"), 1);
+        addBuildingToPool(templatePools, processorLists, savannaPoolRL, RLUtils.createRL("village/houses/savanna_bbq_camp_1"), 2);
+        addBuildingToPool(templatePools, processorLists, desertPoolRL, RLUtils.createRL("village/houses/desert_bbq_camp_1"), 2);
+        addBuildingToPool(templatePools, processorLists, taigaPoolRL, RLUtils.createRL("village/houses/taiga_bbq_camp_1"), 2);
+        addBuildingToPool(templatePools, processorLists, snowyPoolRL, RLUtils.createRL("village/houses/snowy_bbq_camp_1"), 3);
+        addBuildingToPool(templatePools, processorLists, snowyPoolRL, RLUtils.createRL("village/houses/snowy_bbq_camp_2"), 1);
     }
 
-    public static void addBuildingToPool(Registry<StructureTemplatePool> templatePoolRegistry, ResourceLocation poolRL, ResourceLocation nbtPieceRL, int weight) {
+    public static void addBuildingToPool(Registry<StructureTemplatePool> templatePoolRegistry, Registry<StructureProcessorList> processorListRegistry, ResourceLocation poolRL, ResourceLocation nbtPieceRL, int weight) {
         var pool = templatePoolRegistry.get(poolRL);
         if (pool == null) {
             return;
         }
-        var piece = SinglePoolElement.single(nbtPieceRL.toString()).apply(StructureTemplatePool.Projection.RIGID);
+        var emptyProcessorList = processorListRegistry.getHolderOrThrow(EMPTY_PROCESSOR_LIST_KEY);
+        var piece = SinglePoolElement.single(nbtPieceRL.toString(), emptyProcessorList).apply(StructureTemplatePool.Projection.RIGID);
         pool.rawTemplates = Util.make(new ArrayList<>(pool.rawTemplates), newRawTemplates -> newRawTemplates.add(Pair.of(piece, weight)));
         for (var i = 0; i < weight; i++) {
             pool.templates.add(piece);
