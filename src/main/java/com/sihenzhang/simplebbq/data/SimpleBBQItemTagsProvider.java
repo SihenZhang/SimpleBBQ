957047ca7d186e71b662a2564fa80200d8a8847e
SihenZhang <1334125013@qq.com>
2022-08-21 13:21:03 +0800

    Cooked Skewer cannot be seasoned

--- src/main/java/com/sihenzhang/simplebbq/data/SimpleBBQItemTagsProvider.java
+++ src/main/java/com/sihenzhang/simplebbq/data/SimpleBBQItemTagsProvider.java
@@ -24,51 +24,30 @@ public class SimpleBBQItemTagsProvider extends ItemTagsProvider {
         this.tag(SimpleBBQItemTags.SKEWER).addTag(Tags.Items.RODS_WOODEN).add(Items.BAMBOO);
         this.tag(SimpleBBQItemTags.SEASONING).add(Items.HONEY_BOTTLE, SimpleBBQRegistry.CHILI_POWDER.get(), SimpleBBQRegistry.CUMIN.get(), SimpleBBQRegistry.SALT_AND_PEPPER.get());
 
-        var allSkewers = ImmutableSet.of(
+        var allRawSkewers = ImmutableSet.of(
                 SimpleBBQRegistry.BEEF_SKEWER.get(),
-                SimpleBBQRegistry.COOKED_BEEF_SKEWER.get(),
                 SimpleBBQRegistry.CHICKEN_SKEWER.get(),
-                SimpleBBQRegistry.COOKED_CHICKEN_SKEWER.get(),
                 SimpleBBQRegistry.MUTTON_SKEWER.get(),
-                SimpleBBQRegistry.COOKED_MUTTON_SKEWER.get(),
                 SimpleBBQRegistry.PORK_SKEWER.get(),
-                SimpleBBQRegistry.COOKED_PORK_SKEWER.get(),
                 SimpleBBQRegistry.RABBIT_SKEWER.get(),
-                SimpleBBQRegistry.COOKED_RABBIT_SKEWER.get(),
                 SimpleBBQRegistry.COD_SKEWER.get(),
-                SimpleBBQRegistry.COOKED_COD_SKEWER.get(),
                 SimpleBBQRegistry.SALMON_SKEWER.get(),
-                SimpleBBQRegistry.COOKED_SALMON_SKEWER.get(),
                 SimpleBBQRegistry.BREAD_SLICE_SKEWER.get(),
-                SimpleBBQRegistry.TOAST_SKEWER.get(),
                 SimpleBBQRegistry.MUSHROOM_SKEWER.get(),
-                SimpleBBQRegistry.ROASTED_MUSHROOM_SKEWER.get(),
-                SimpleBBQRegistry.POTATO_SKEWER.get(),
-                SimpleBBQRegistry.BAKED_POTATO_SKEWER.get()
+                SimpleBBQRegistry.POTATO_SKEWER.get()
         );
         var cannotBeSeasonedByHoney = ImmutableSet.of(
                 SimpleBBQRegistry.COD_SKEWER.get(),
-                SimpleBBQRegistry.COOKED_COD_SKEWER.get(),
-                SimpleBBQRegistry.SALMON_SKEWER.get(),
-                SimpleBBQRegistry.COOKED_SALMON_SKEWER.get()
+                SimpleBBQRegistry.SALMON_SKEWER.get()
         );
-        var cannotBeSeasonedByChiliPowder = ImmutableSet.of(
-                SimpleBBQRegistry.BREAD_SLICE_SKEWER.get(),
-                SimpleBBQRegistry.TOAST_SKEWER.get()
-        );
-        var cannotBeSeasonedByCumin = ImmutableSet.of(
-                SimpleBBQRegistry.BREAD_SLICE_SKEWER.get(),
-                SimpleBBQRegistry.TOAST_SKEWER.get()
-        );
-        var cannotBeSeasonedBySaltAndPepper = ImmutableSet.of(
-                SimpleBBQRegistry.BREAD_SLICE_SKEWER.get(),
-                SimpleBBQRegistry.TOAST_SKEWER.get()
-        );
+        var cannotBeSeasonedByChiliPowder = ImmutableSet.of(SimpleBBQRegistry.BREAD_SLICE_SKEWER.get());
+        var cannotBeSeasonedByCumin = ImmutableSet.of(SimpleBBQRegistry.BREAD_SLICE_SKEWER.get());
+        var cannotBeSeasonedBySaltAndPepper = ImmutableSet.of(SimpleBBQRegistry.BREAD_SLICE_SKEWER.get());
 
-        this.tag(SimpleBBQItemTags.CAN_BE_SEASONED_BY_HONEY).add(allSkewers.stream().filter(item -> !cannotBeSeasonedByHoney.contains(item)).toArray(Item[]::new));
-        this.tag(SimpleBBQItemTags.CAN_BE_SEASONED_BY_CHILI_POWDER).add(allSkewers.stream().filter(item -> !cannotBeSeasonedByChiliPowder.contains(item)).toArray(Item[]::new));
-        this.tag(SimpleBBQItemTags.CAN_BE_SEASONED_BY_CUMIN).add(allSkewers.stream().filter(item -> !cannotBeSeasonedByCumin.contains(item)).toArray(Item[]::new));
-        this.tag(SimpleBBQItemTags.CAN_BE_SEASONED_BY_SALT_AND_PEPPER).add(allSkewers.stream().filter(item -> !cannotBeSeasonedBySaltAndPepper.contains(item)).toArray(Item[]::new));
+        this.tag(SimpleBBQItemTags.CAN_BE_SEASONED_BY_HONEY).add(allRawSkewers.stream().filter(item -> !cannotBeSeasonedByHoney.contains(item)).toArray(Item[]::new));
+        this.tag(SimpleBBQItemTags.CAN_BE_SEASONED_BY_CHILI_POWDER).add(allRawSkewers.stream().filter(item -> !cannotBeSeasonedByChiliPowder.contains(item)).toArray(Item[]::new));
+        this.tag(SimpleBBQItemTags.CAN_BE_SEASONED_BY_CUMIN).add(allRawSkewers.stream().filter(item -> !cannotBeSeasonedByCumin.contains(item)).toArray(Item[]::new));
+        this.tag(SimpleBBQItemTags.CAN_BE_SEASONED_BY_SALT_AND_PEPPER).add(allRawSkewers.stream().filter(item -> !cannotBeSeasonedBySaltAndPepper.contains(item)).toArray(Item[]::new));
     }
 
     @Override
